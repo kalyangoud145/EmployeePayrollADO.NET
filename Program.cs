3e7036a73986198d87952b9d466a690fb281b4c5
kalyangoud145 <kalyangoud145@gmail.com>
2020-11-23 17:07:22 +0530

    [Kalyan] added data to table

--- Program.cs
+++ Program.cs
@@ -6,8 +6,20 @@ namespace EmployeePayrollADO.NET
     {
         static void Main(string[] args)
         {
+            EmployeeModel employeeModel = new EmployeeModel();
             EmployeeRepo repo = new EmployeeRepo();
-            repo.GetAllEmployee();
+            employeeModel.Name = "Keerthi";
+            employeeModel.Salary = 50000;
+            employeeModel.Gender = 'F';
+            employeeModel.PhoneNumber = "864023030";
+            employeeModel.Address = "HYD";
+            employeeModel.Department = "CiVils";
+            employeeModel.basic_Pay = 30000;
+            employeeModel.Deductions = 12000;
+            employeeModel.Taxable_Pay = 40000;
+            employeeModel.Income_tax = 7000;
+            employeeModel.Net_Pay = 41000;
+            repo.AddEmployee(employeeModel);
         }
     }
 }
