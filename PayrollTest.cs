f8a5932f62df41a7fd140b3229b96ecafd7b1202
kalyangoud145 <kalyangoud145@gmail.com>
2020-11-23 19:57:42 +0530

    [Kalyan] implemented inbuilt functions sum avg count etc..

--- PayrollTest.cs
+++ PayrollTest.cs
@@ -11,13 +11,13 @@ namespace EmployeePayrollTest
         /// table should return true if data is updated.
         /// </summary>
         [TestMethod]
-            public void GivenPayrollToUpdate_WhenDataisPresentInTable_ShouldReturnTrueIfDataIsUpdated()
-            {
-                // Act
-                EmployeeRepo repo = new EmployeeRepo();
-                bool Emp_BasicPay = repo.UpdateTables("update Employee_Payroll set basic_Pay = 3000000.00 where Id=5");
-                // Assert
-                Assert.AreEqual(Emp_BasicPay, true);
-            }
+        public void GivenPayrollToUpdate_WhenDataisPresentInTable_ShouldReturnTrueIfDataIsUpdated()
+        {
+            // Act
+            EmployeeRepo repo = new EmployeeRepo();
+            bool Emp_BasicPay = repo.UpdateTables("update Employee_Payroll set basic_Pay = 3000000.00 where Id=5");
+            // Assert
+            Assert.AreEqual(Emp_BasicPay, true);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,18 +8,15 @@ namespace EmployeePayrollADO.NET
         {
             EmployeeModel employeeModel = new EmployeeModel();
             EmployeeRepo repo = new EmployeeRepo();
-            employeeModel.Name = "Keerthi";
-            employeeModel.Salary = 50000;
-            employeeModel.Gender = 'F';
-            employeeModel.PhoneNumber = "864023030";
-            employeeModel.Address = "HYD";
-            employeeModel.Department = "CiVils";
-            employeeModel.basic_Pay = 30000;
-            employeeModel.Deductions = 12000;
-            employeeModel.Taxable_Pay = 40000;
-            employeeModel.Income_tax = 7000;
-            employeeModel.Net_Pay = 41000;
-            repo.AddEmployee(employeeModel);
+            repo.SumOfSalaryGenderWise();
+            Console.WriteLine("******");
+            repo.AverageOfSalaryGenderWise();
+            Console.WriteLine("******");
+            repo.MinimumSalaryGenderWise();
+            Console.WriteLine("******");
+            repo.MaximumSalaryGenderWise();
+            Console.WriteLine("******");
+            repo.CountOfEmployeesGenderWise();
         }
     }
 }
